cce4f6369ab13c45f791bc6e3d53a9857cbb2ead
Ubaeby <christianchoi45@gmail.com>
2022-12-03 17:32:10 -0800

    Added birthday function using useState

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+const PersonCard = props => {
+    const { firstName, lastName, age, hairColor } = props;
+    const [newAge, setNewAge] = useState({
+        ageCount: age
+    });
 
-const PersonCard = (props) => {
-    const {firstName, lastName, age, hairColor} = props;
+    const ageClick = () => {
+        setNewAge({
+            ageCount: newAge.ageCount + 1
+        });
+    }
+
+    const restart = () => {
+        setNewAge({
+            ageCount : age
+        });
+    }
+
     return (
         <div>
-            <h1>
-            { lastName }, { firstName }
-            </h1>
-            <p>
-                Age: { age }
-            </p>
-            <p>
-                Hair Color: { hairColor}
-            </p>
+            <div>
+                <h1>
+                    {lastName}, {firstName}
+                </h1>
+                <p>
+                    Age: {newAge.ageCount}
+                </p>
+                <p>
+                    Hair Color: {hairColor}
+                </p>
+
+                <button onClick={ageClick}> Birthday Button for {firstName} {lastName} </button>
+            </div>
+            <div>
+                <button onClick={restart}> Go back to your original age!</button>
+            </div>
         </div>
     )
 }
